352ae0c706f6ea694733d7f7bbe25435c6f1c788
HighRiseCode <highrisecode@gmail.com>
2018-10-27 17:49:56 -0500

    Move game component into game container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GameContainer from './container/GameContainer.js';
+
+class App extends React.Component{
+  render(){
+    return(
+      <GameContainer></GameContainer>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
